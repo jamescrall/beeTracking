2eadcf60b4d0a819d320ea2594e6c2c536862e6f
jamescrall <james.crall@gmail.com>
2019-02-07 10:01:48 -0500

    bug fixes

--- BEECH_General/fixShortNanGaps.m
+++ BEECH_General/fixShortNanGaps.m
@@ -1,22 +1,27 @@
-function intData = fixShortNanGaps(inputData, maxGapSize)
-%Code to interpolated NaN gaps in a single dimension for a determined gap
-%interpolates missing data (nans) along columns of "inputData" up to a
-%maximum gap size of "maxGapSize"
-
-%%
-for i = 2:numel(inputData)
+function inputMat = fixShortNanGaps(inputMat, maxGapSize)
+    %Code to interpolated NaN gaps in a single dimension for a determined gap
+    %interpolates missing data (nans) along columns of "inputData" up to a
+    %maximum gap size of "maxGapSize"
+    
     %%
-    if isnan(inputData(i)) & ~isnan(inputData(i-1)) %If current step is nan, and previous step isn't, try to heal
-        tVec = inputData(i:end);
-        nextRealNumberInd = min(find(~isnan(tVec)));
-        if nextRealNumberInd <= maxGapSize + 1
-            t1 = i-1;
-            t2 = i + nextRealNumberInd -1;
-            inputData(t1:t2) = linspace(inputData(t1), inputData(t2), nextRealNumberInd +1);
+    for j = 1:size(inputMat,2)
+        inputData = inputMat(:,j)
+        for i = 2:numel(inputData)
+            %%
+            if isnan(inputData(i)) & ~isnan(inputData(i-1)) %If current step is nan, and previous step isn't, try to heal
+                tVec = inputData(i:end);
+                nextRealNumberInd = min(find(~isnan(tVec)));
+                if nextRealNumberInd <= maxGapSize + 1
+                    t1 = i-1;
+                    t2 = i + nextRealNumberInd -1;
+                    inputData(t1:t2) = linspace(inputData(t1), inputData(t2), nextRealNumberInd +1);
+                end
+                
+            end
+            
         end
-        
+        clear tVex;
+        clear nextRealNumberInd;
+        inputMat(:,j) = inputData; %Write data back into matrix
     end
-end
-
-intData = inputData;
-
+    
